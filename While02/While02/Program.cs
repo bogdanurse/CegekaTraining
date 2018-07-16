28874248d0b5e223810281ef96fd9ef633354bec
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 14:34:19 +0200

    While02
    Implement While02 Exemple

---
+++ While02/While02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum =0;
+            int number = 0;
+            do
+            {
+                number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+
+            } while (number != 0);
+
+            Console.WriteLine("The sum is : {0}", sum);
+        }
+    }
+}
