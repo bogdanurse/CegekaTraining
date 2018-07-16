5c732421a983a613d7a726c06474484352acc5d9
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 13:44:31 +0200

    If01
    First If condition

---
+++ If01/If01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If01
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your age is : ");
+            var age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your sex is : male/female ");
+            var sex = Console.ReadLine();
+
+            if ((sex == "male") && (age == 24))
+            {
+                Console.WriteLine("Valid person");
+            }
+            else if ((sex == "female") && (age >20) && (age <32))
+            {
+                Console.WriteLine("Valid person");
+            }
+            else
+            {
+                Console.WriteLine("Invalid person");
+            }
+        }
+    }
+}
