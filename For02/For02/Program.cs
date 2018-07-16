aeb871fcf0246eca895a28a8f5f884733817b996
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 15:54:27 +0200

    For 02 and For 03
    Resolving exercise

---
+++ For02/For02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =0;i<20;i++)
+            {
+                for(int j=20;j<40;j++)
+                {
+                    int result = i * j;
+                    Console.WriteLine("The results are : {0}", result);
+                }
+            }
+        }
+    }
+}
