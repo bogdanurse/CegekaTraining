48c0d7da06a8cbbf8113b4b88a0c5056d9026f67
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 12:02:33 +0200

    Created Function
    Created a function to generate the string

--- Calculation/Calculation/Program.cs
+++ Calculation/Calculation/Program.cs
@@ -12,20 +12,37 @@ namespace Calculation
 		{
 			int kilometers = 10;
 			double time = 1.5;
-			Boolean truth = true;
+			bool truth = true;
 			string name = "Gigi";
 			string name1 = "Mike";
 
-			Console.WriteLine("{0} has ran {1} in {2} hours. It is {3} that he's a family man." +
-				"He takes his brother out for a drink at a bar called {4}'s Han. " +
-				"He and his wife are going to have a son. His name will be {5}, just like his father's. " +
-				"{6} dreams to get a diploma from his son saying '{7}, father of the year'.", name, kilometers, time, truth, name, name, name, name);
+			string result = CreateOutput(kilometers, time, truth, name);
+			Console.WriteLine(result);
 
-			Console.WriteLine("{0} has ran {1} in {2} hours. It is {3} that he's a family man." +
-				"He takes his brother out for a drink at a bar called {4}'s Han. " +
-				"He and his wife are going to have a son. His name will be {5}, just like his father's. " +
-				"{6} dreams to get a diploma from his son saying '{7}, father of the year'.", name1, kilometers, time, truth, name1, name1, name1, name1);
+			string result1 = CreateOutput(kilometers, time, truth, name1);
+			Console.WriteLine(result1);
 
+
+			//Console.WriteLine("{0} has ran {1} in {2} hours. It is {3} that he's a family man." +
+			//	"He takes his brother out for a drink at a bar called {4}'s Han. " +
+			//	"He and his wife are going to have a son. His name will be {5}, just like his father's. " +
+			//	"{6} dreams to get a diploma from his son saying '{7}, father of the year'.", name, kilometers, time, truth, name, name, name, name);
+
+			//Console.WriteLine("{0} has ran {1} in {2} hours. It is {3} that he's a family man." +
+			//	"He takes his brother out for a drink at a bar called {4}'s Han. " +
+			//	"He and his wife are going to have a son. His name will be {5}, just like his father's. " +
+			//	"{6} dreams to get a diploma from his son saying '{7}, father of the year'.", name1, kilometers, time, truth, name1, name1, name1, name1);
+
+		}
+		
+
+		public static string CreateOutput(int km, double t, bool trut, string name)
+		{
+			string output;
+			output = name + " has ran " + km + " kilometers in " + t + " hours. It is " + trut + " that he's a family man." +
+				" He takes his brother out for a drink at a bar called " + name + "s Han. He and his wife are going to have a son. His name will be " + name + ", just like his father's. " +
+				name + " dreams to get a diploma from his son saying '" + name + ", father of the year'.\n";
+			return output;
 		}
 	}
 }
