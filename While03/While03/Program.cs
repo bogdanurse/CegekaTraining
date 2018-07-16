54d75101e009dcc3274c82e19b10955dfed60d0a
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 15:34:33 +0200

    While03
    Resolve exercise

---
+++ While03/While03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, 100);
+            Console.WriteLine("The number is {0}.", number);
+            int insertednumber = 0;
+            do
+            {
+                Console.WriteLine("Guess the number that is random generated.");
+                insertednumber = int.Parse(Console.ReadLine());
+                if (number > insertednumber)
+                {
+                    Console.WriteLine("It's bigger");
+                } else
+                if (number < insertednumber)
+                {
+                    Console.WriteLine("It's smaller");
+                }
+
+            } while (number != insertednumber);
+            Console.WriteLine("Your guess is correct! Congrats!");
+        }
+    }
+}
