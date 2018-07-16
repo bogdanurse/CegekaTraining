51dc65eb089a4f7c1044dba5f493b685e0ac56ff
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 15:58:38 +0200

    Break01
    Resolve Exercise

---
+++ Break01/Break01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            while (true)
+            {
+                
+                Console.WriteLine("Insert a number : ");
+                var readNumber = int.Parse(Console.ReadLine());
+                sum += readNumber;
+                if (sum > 100)
+                {
+                    Console.WriteLine("The sum is over 100");
+                    break;
+                }
+
+            }
+        }
+    }
+}
