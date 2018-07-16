cbef08ccda477b76a28b946d4c62aa75394d31a7
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 16:32:15 +0200

    SwitchOnEnum
    Exercise

---
+++ Switch01/Switch01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch01
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a name : ");
+            var name = Console.ReadLine().ToLower();
+      
+            switch (name)
+            {
+                case "alex":
+                    Console.WriteLine("Alex likes skiing");
+                    break;
+                case "bogdan":
+                    Console.WriteLine("Bogdan likes football");
+                    break;
+                case "marcel":
+                    Console.WriteLine("Marcel likes handball");
+                    break;
+                  default:
+                    Console.WriteLine("I don't know anything about {0}",name);
+                    break;
+            }
+        }
+    }
+}
---
+++ SwitchOnEnum/SwitchOnEnum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchOnEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Today;
+            Console.WriteLine("Today's date is {0}", date);
+            float value = date.Month;
+
+            switch(value)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("It's Winter");
+                    break;            
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("It's Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("It's Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("It's Autumn");
+                    break;
+               
+            }
+        }
+    }
+}
