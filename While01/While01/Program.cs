f63a1e24ce52608cfb2a188c5ab0feb58f50491a
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-16 14:26:57 +0200

    While01
    Exemple created

---
+++ While01/While01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.Write("Please add a number : ");
+            int value = int.Parse(Console.ReadLine());
+            while(i<=value)
+            {
+                Console.WriteLine("The value is {0}\n",i);
+                i++;
+                //if we don't increment the i, we will have an infinite loop showing 0 
+            }
+
+        }
+    }
+}
