7a52868359b9e0d6135d600c822214ba4f308b41
Bogdan Urse <bogdan.urse@cegeka.com>
2018-07-17 12:51:25 +0200

    added unit test
    unit test added

---
+++ Objects/ObjectsTests/PointTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects.Tests
+{
+    [TestClass()]
+    public class PointTests
+    {
+        [TestMethod()]
+        public void DistanceToTest()
+        {
+            Point punctA = new Point(1, 3);
+            Point punctB = new Point(2, 4);
+
+            Assert.AreEqual(punctA.DistanceTo(punctB), 1,4142135623731);
+            
+        }
+    }
+}
